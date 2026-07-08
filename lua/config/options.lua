18e5d2cfaffe1b002ece17d180ceeeb66a852f63
Christofanis Skordas <skordasc@uchicago.edu>
2026-07-08 17:09:21 -0500

    added some vim options

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,4 +2,9 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.undofile = true
+vim.opt.scrolloff = 16
 
